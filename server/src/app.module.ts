6d85ea554de05eaa68f8f150bca2dbd04bd69cde
Mirian97 <mirianquispe1505@gmail.com>
2025-11-13 08:30:57 -0300

    feat: configure environment variables and database connection in AppModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
+
+console.log(
+  'AppModule initialized with database connection settings.',
+  process.env.DB_HOST,
+  process.env.DB_PORT,
+  typeof process.env.DB_PORT,
+);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
