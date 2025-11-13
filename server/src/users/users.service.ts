71014d7737afdcd5d6026e73fe973f19b8eee620
Mirian97 <mirianquispe1505@gmail.com>
2025-11-13 19:26:46 -0300

    feat(users): implement user creation, update, and retrieval functionality

---
+++ server/src/users/users.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { UserNotFoundException } from './exceptions/user-not-found.exception';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
+  }
+
+  async findAll() {
+    return await this.userRepository.find();
+  }
+
+  async findOne(id: number, withPassword: boolean = false) {
+    const selectQueryBuilder = this.userRepository
+      .createQueryBuilder('user')
+      .where('user.id = :id', { id });
+    if (withPassword) {
+      selectQueryBuilder.addSelect('user.password');
+    }
+    const user = await selectQueryBuilder.getOne();
+    if (!user) {
+      throw new UserNotFoundException();
+    }
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    await this.userRepository.update(id, updateUserDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+    await this.userRepository.delete(id);
+  }
+}
