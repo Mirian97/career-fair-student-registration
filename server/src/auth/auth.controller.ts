25c1c2044147a12c300dfb3fe7af56acc60157b4
Mirian97 <mirianquispe1505@gmail.com>
2025-11-14 12:45:15 -0300

    feat: add insomnia collection for career fair
    - Added requests for users, schools
    - Added requests for students
    - Configured base environment

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -19,7 +19,6 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Public()
   @Post('register')
   @Roles(Role.Admin)
   register(@Body() signupDto: CreateUserDto) {
